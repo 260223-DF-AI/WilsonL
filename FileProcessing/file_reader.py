75b931dc6bc67e0354edb9c400e8df0d48d8a346
leew735 <ameliaw735@revature.net>
2026-03-03 15:59:39 -0600

    task 1-2 drafted

--- FileProcessing/file_reader.py
+++ FileProcessing/file_reader.py
@@ -16,15 +16,19 @@ def read_csv_file(filepath):
     Raises: FileProcessingError with descriptive message
     """
     logger.debug(f"Attempting to read csv file {filepath}")
-    d_list = []
+    d_list = [] # list to return
+
     try:
         with open(filepath, "r") as f:
+            # split the contents by line
             contents = f.read().split("\n")
             logger.debug("File read and split")
+
+            # go through the file, line by line
             for i, line in enumerate(contents):
                 logger.debug(f"Line {i}: {line}")
                 if i == 0:
-                    continue
+                    continue # ignore column names
                 entry = {
                     "date": line[0],
                     "store_id": line[1],
@@ -32,19 +36,23 @@ def read_csv_file(filepath):
                     "quantity": line[3],
                     "price": line[4]
                 }
+
                 d_list.append(entry)
                 logger.debug(f"Entry appended: {entry}")
     except FileNotFoundError as e:
-        logger.error(e)
+        logger.error(f"File not found error: {e}")
+    except UnicodeDecodeError as e:
+        logger.error(f"UnicodeDecodeError: {e}")
     except exceptions.FileProcessingError as e:
-        logger.error(e)
+        logger.error(f"FileProcessingError: {e}")
     except Exception as e:
-        logger.error(e)
+        logger.error(f"Other exception: {e}")
     else:
         logger.info(f"File successfully read:  {filepath}")
     return d_list
 
-read_csv_file("WilsonL/FileProcessing/data/nice_sales.csv")
-read_csv_file("WilsonL/FileProcessing/data/sample_sales.csv")
-read_csv_file("nosuchfile.py")
-print(read_csv_file("WilsonL/FileProcessing/data/empty_file.csv"))
+# test cases
+# read_csv_file("WilsonL/FileProcessing/data/nice_sales.csv") #.csv with perfect sales data
+# read_csv_file("WilsonL/FileProcessing/data/sample_sales.csv")
+# read_csv_file("nosuchfile.py") # no such file
+# read_csv_file("WilsonL/FileProcessing/data/empty_file.csv") # empty file
