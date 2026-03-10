7d7ddd79620baaf902d80e0173c1fc94948b1141
leew735 <ameliaw735@revature.net>
2026-03-10 15:11:04 -0500

    Finished Employees

--- Employees/task_scripts.sql
+++ Employees/task_scripts.sql
@@ -79,8 +79,12 @@ SELECT emp_id, first_name, last_name, salary FROM employees WHERE salary > (SELE
 SELECT dept_name, COUNT(DISTINCT project_id) as project_count FROM departments d
     JOIN projects p ON d.dept_id = p.dept_id GROUP BY d.dept_id;
 -- 4.3
-SELECT * FROM employees 
+SELECT dept_id, MAX(salary) AS max_salary FROM employees GROUP BY dept_id;
 
+-- 4.4
+SELECT EXTRACT(YEAR FROM age(CURRENT_DATE, hire_date)) AS years, 
+EXTRACT(MONTH FROM age(CURRENT_DATE, hire_date)) AS months, 
+first_name, last_name, emp_id FROM employees;
 
 SELECT * FROM employees;
 SELECT * FROM departments;
